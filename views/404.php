828ec52cb785a3de0f4a9cd0b6737b922d97e6e9
Thomas Vanwelden <vanweldenthomas@live.fr>
2015-12-10 02:36:33 +0100

    Modification de la page 404.php

--- views/404.php
+++ views/404.php
@@ -1,50 +1,22 @@
+<?php include_once('views/layout/header.inc.php'); ?>
+<div class="container img-full">
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+<h1 class="header center white-text">This page is under construction</h1>
 
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-      <link href="css/custom.css" rel="stylesheet" type="text/css">
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
-
-    <body>
-
-       <?php include("header.inc.php"); ?>
-
-      <div class="container img-full">
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-                </br>
-
-                <h1 class="header center white-text">This page is under construction</h1>
-
-                <div class="row center">
-                  <h5 class="header col s12 light white-text">Volunteers is a new website, we are developing new features that we hope you will enjoy</h5>
-                </div>
-                </br>
-                <div class="row center">
-                  <a href="index.html" class="btn btn-blue howitworks">Go back to homepage</a>
-                </div>
-      </div>
-      
-
-
-
-
-      
-      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
-        
+    <div class="row center">
+      <h5 class="header col s12 light white-text">Volunteers is a new website, we are developing new features that we hope you will enjoy</h5>
+    </div>
+    </br>
+    <div class="row center">
+      <a href="?" class="btn btn-blue howitworks">Go back to homepage</a>
+    </div>
+</div>
+<?php include_once('views/layout/footer.inc.php'); ?>
