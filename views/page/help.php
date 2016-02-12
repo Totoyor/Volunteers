cc67c3d54dbbb9984260cb7deb97c94c05670eb6
Thomas Vanwelden <vanweldenthomas@live.fr>
2016-02-12 16:00:39 +0100

    Ajout de la page help

--- views/page/help.php
+++ views/page/help.php
@@ -1,11 +1,155 @@
 <?php include_once('views/layout/header.inc.php'); ?>
-    <div class="container content-blog">
-        <div class="row">
-            <div class="col s12 m12">
-                <div class="card-panel center">
-                    <h1>You don't need help</h1>
+    
+<div class="container-full bghelp valign-wrapper center">
+            <div class="row">
+                <div class="col l12">
+                    <h1>The best way to find volunteers<br/> for your event</h1>
+                </div>
+                
+                <a href="event/home" class="btn btn-menu-bis btn-hoverbleu">Start now</a>
+            </div> 
+        </div>
+        
+        <div class="container">
+            <div class="page-content">
+                <div class="help-title center">
+                    <h2><strong>How it works ?</strong></h2>
+                    <h3>Are you a volunteer or an organizer ?</h3>
+                </div>
+                
+                <div class="row nospace">
+                    <div class="col s8 offset-s2">
+                        <ul class="tabs tabsnobg">
+<li class="tab col s4"><a class="active" href="#help1">I want to volunteer</a></li>
+<li class="tab col s4"><a href="#help2">I am an organizer</a></li>
+
+
+                        </ul>
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+          <div class="container-full whitebg">
+            <div class="page-content">
+                
+                <div id="help1">
+                <div class="row center">
+                    <div class="col l4 s12 space4">
+                        <i class="medium material-icons blue-icon">favorite_border</i>
+                        <h4 class="title-parts-help">Participate</h4>
+                        <p>at your favourite events 
+for free</p>
+                    </div>
+                    
+                    <div class="col l4 s12 space4">
+                        <i class="medium material-icons blue-icon">people_outline
+</i>
+                        <h4 class="title-parts-help">Meet</h4>
+                        <p>awesome people during your work as a volunteer</p>
+                    </div>
+                    
+                    <div class="col l4 s12 space4">
+                        <i class="medium material-icons blue-icon">mood</i>
+                        <h4 class="title-parts-help">Live</h4>
+                        <p>a new experience for each event and each mission !</p>
+                    </div>
+                </div> 
+
+                            <h3 class="faqtitle center">FAQ</h3>
+                            <hr class="hrblue">
+  
+                    <div class="row">
+                        <div class="col l8 s12 offset-s2 left-align">
+                             <ul class="collapsible" data-collapsible="accordion">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">keyboard_arrow_down</i>How do I apply to an event ?</div>
+      <div class="collapsible-body"><p>Easy : you just have to browse for an event that you like on the homepage or using the search engine of the events page.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">keyboard_arrow_down</i>Is volunteers only for adults people ?</div>
+      <div class="collapsible-body"><p>Sadly, yes. You must be +18 to be a volunteer.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">keyboard_arrow_down</i>Third</div>
+      <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+    </li>
+  </ul>
+                        </div>
+                    </div>
+              </div>
+                
+                
+                
+                <div id=help2>
+                <div class="row center">
+                    <div class="col l4 s12 space4">
+                        <i class="medium material-icons orange-icon">attach_money</i>
+                        <h4 class="title-parts-help">Save time</h4>
+                        <p>with Volunteers, we're here to bring volunteers to you !</p>
+                    </div>
+                    
+                    <div class="col l4 s12 space4">
+                        <i class="medium material-icons orange-icon">people_outline
+</i>
+                        <h4 class="title-parts-help">Hire</h4>
+                        <p>the best volunteers to help you to succeed an awesome event</p>
+                    </div>
+                    
+                    <div class="col l4 s12 space4">
+                        <i class="medium material-icons orange-icon">trending_up</i>
+                        <h4 class="title-parts-help">Better visibility</h4>
+                        <p>for your event</p>
+                    </div>
+                    
+                </div> 
+                
+                <div class="row center">
+                    <div class="col l4 s12 space4">
+                        <i class="medium material-icons orange-icon">add_circle_outline</i>
+                        <h4 class="title-parts-help">Create your event</h4>
+                        <p>in the simplest and easiest way</p>
+                    </div>
+                    
+                    <div class="col l4 s12 space4">
+                        <i class="medium material-icons orange-icon">people_outline
+</i>
+                        <h4 class="title-parts-help">Check volunteers</h4>
+                        <p>who applied to your event</p>
+                    </div>
+                    
+                    <div class="col l4 s12 space4">
+                        <i class="medium material-icons orange-icon">mood</i>
+                        <h4 class="title-parts-help">Enjoy your event</h4>
+                        <p></p>
+                    </div>
+                    
+                </div>
+
+                            <h3 class="faqtitle center">FAQ</h3>
+                            <hr class="hrorange">
+   
+                    <div class="row">
+                        <div class="col l8 s12 offset-s2 left-align">
+                             <ul class="collapsible" data-collapsible="accordion">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">keyboard_arrow_down</i>How do I create an event ?</div>
+      <div class="collapsible-body"><p>It's simple. You just need to fill the form by following the 3 steps to make your event live.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">keyboard_arrow_down</i>Can I edit or delete an event ?</div>
+      <div class="collapsible-body"><p>Of course. You can edit your event whenever you want and you can delete it as soon as it is over.</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">keyboard_arrow_down</i>Who are the volunteers ?</div>
+      <div class="collapsible-body"><p>The volunteers on our website are people who search a new experience to live by working with you and especially helping you to built the greatest event. They can be skilled or not but don't worry about that, they are all very motivated.</p></div>
+    </li>
+  </ul>
+                        </div>
+                    </div>    
+                </div><!-- fin help2-->
+            </div>
+          </div>
+   
+
 <?php include_once('views/layout/footer.inc.php'); ?>
