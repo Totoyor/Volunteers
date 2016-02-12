65741a365f8b9f0b4a9518fe878eb70b0d0151e8
jordanlamantia <jordan.lamantia@eemi.com>
2016-02-12 16:40:12 +0100

    Dessign

--- views/user/change_password.php
+++ views/user/change_password.php
@@ -1,11 +1,5 @@
 <?php include_once('views/layout/header.inc.php'); ?>
-<<<<<<< HEAD
-
-    
- <div class="container content-validate">
-=======
-    <div class="container content-blog">
->>>>>>> 05feda4084ea4fdf66fba4b1553660321e7fd964
+<div class="container content-validate">
         <div class="row">
             <div class="col m6 offset-m3 offset-l3 center_content">
                 <div class="card-panel center">
@@ -42,9 +36,4 @@
             </div>
         </div>
     </div>
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 05feda4084ea4fdf66fba4b1553660321e7fd964
 <?php include_once('views/layout/footer.inc.php'); ?>
