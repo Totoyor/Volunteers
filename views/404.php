a1e7c3ab6e18e08eb339c1118b3652c6f4629793
Thomas Vanwelden <vanweldenthomas@live.fr>
2016-02-15 12:55:42 +0100

    Modifications css sur la homepage et la creation events

--- views/404.php
+++ views/404.php
@@ -1,7 +1,19 @@
 <?php include_once('views/layout/header.inc.php'); ?>
 
-<h1 class="title_404">ERROR 404</h1>
-
-<p>Cette page n'existe pas !</p>
+    <div class="page-contact">
+        <div class="page-content margtop100">
+            <div class="row">
+                <div class="page-content">
+                    <div class="col l6 m12 s12 center margtop-responsive">
+                        <h1 class="title-section margleft"><strong>ERROR 404</strong></h1>
+                        <p><b>Please don't do this !</b></br></br> This page doesn't exist, clic on the logo to return on the homepage</p>
+                    </div>
+                    <div class="col l6 m12 s12">
+                        <iframe src="//giphy.com/embed/dlMIwDQAxXn1K?html5=true&hideSocial=true" width="480" height="269" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php include_once('views/layout/footer.inc.php'); ?>
